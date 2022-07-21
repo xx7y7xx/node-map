e3acbabf5ce9bbe550fe0b68d5fa68a6e5533daa
Devin Chen <5204106+xxd3vin@users.noreply.github.com>
2022-07-21 22:54:43 +0800

    Show js eval err

--- src/NodeEditor/TransformEvalComponent.js
+++ src/NodeEditor/TransformEvalComponent.js
@@ -3,23 +3,41 @@ import { jsonSocket } from './JsonComponent';
 import TextAreaControl from './TextAreaControl';
 import DivControl from './DivControl';
 
+const log = (...args) => {
+  console.debug('TransformEvalComponent', ...args);
+};
 export default class TransformEvalComponent extends Rete.Component {
   constructor() {
     super('Transform Eval');
+
+    // Store all nodes ref related to this component
+    // May use node ref later to add control
+    this.nodes = {
+      // [node.id]: node
+    };
+
+    this.errorMessageControls = {
+      // [node.id]: node
+    };
   }
 
   builder(node) {
+    log('build', node.id);
+    this.nodes[node.id] = node;
+
     const input = new Rete.Input('json', 'Json', jsonSocket);
     const output = new Rete.Output('json', 'Json', jsonSocket);
 
     return node
       .addInput(input)
       .addOutput(output)
-      .addControl(new DivControl('evalScriptLabel', 'Eval Script'))
+      .addControl(new DivControl(`evalScriptLabel[node${node.id}]`, `Eval Script(node:${node.id})`))
       .addControl(new TextAreaControl(this.editor, 'fnStr', node, { cols: 40 }));
   }
 
-  worker(node, inputs, outputs) {
+  worker(nodeData, inputs, outputs) {
+    log('worker', nodeData);
+
     if (inputs.json.length === 0) {
       // there is no input
       return;
@@ -33,12 +51,47 @@ export default class TransformEvalComponent extends Rete.Component {
       return;
     }
 
-    const { controls } = this.editor.nodes.find((n) => n.id === node.id);
+    const { controls } = this.editor.nodes.find((n) => n.id === nodeData.id);
     const fnStr = controls.get('fnStr').getValue();
 
-    const fn = Function('input', fnStr); // eslint-disable-line no-new-func
+    try {
+      // eslint-disable-next-line no-new-func
+      const fn = Function('input', fnStr);
 
-    // eslint-disable-next-line no-param-reassign
-    outputs.json = fn(jsonNodeValue);
+      // eslint-disable-next-line no-param-reassign
+      outputs.json = fn(jsonNodeValue);
+      if (this.errorMessageControls[nodeData.id]) {
+        console.debug('Remove control');
+        this.hideError(nodeData.id);
+      }
+    } catch (err) {
+      console.error('Failed to eval function!', err);
+
+      this.showError(nodeData.id, err);
+    }
+  }
+
+  showError(nodeId, err) {
+    console.debug('Add control', this.nodes[nodeId]);
+
+    const controlKey = `evalScriptErrorMessage[node${nodeId}]`;
+    const node = this.nodes[nodeId];
+
+    if (node.controls.has(controlKey)) {
+      // Control already exists
+      return;
+    }
+
+    this.errorMessageControls[nodeId] = new DivControl(controlKey, `Failed to eval: ${err.message}`);
+    node
+      .addControl(this.errorMessageControls[nodeId])
+      .update(); // Rerender this component
+  }
+
+  hideError(nodeId) {
+    this.nodes[nodeId]
+      .removeControl(this.errorMessageControls[nodeId])
+      .update(); // Rerender this component
+    this.errorMessageControls[nodeId] = null;
   }
 }
