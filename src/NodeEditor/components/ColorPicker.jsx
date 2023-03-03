8bd9846ce1d58c8311ae208e124c6f64b33cb178
devin.chenyang <devin.chenyang@grabtaxi.com>
2023-03-04 00:46:05 +0800

    color picker style

--- src/NodeEditor/components/ColorPicker.jsx
+++ src/NodeEditor/components/ColorPicker.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Input } from 'antd';
+import { Col, Input, Row } from 'antd';
 
 export default function ColorPicker({ label, value, onChange }) {
   return (
-    <div>
-      <span>{label}</span>
-      <Input type="color" value={value} onChange={(e) => onChange(e.target.value)} />
-    </div>
+    <Row style={{
+      margin: '4px 0px',
+    }}
+    >
+      <Col span={8} style={{ lineHeight: '24px' }}>{label}</Col>
+      <Col span={16}><Input size="small" type="color" value={value} onChange={(e) => onChange(e.target.value)} /></Col>
+    </Row>
   );
 }
