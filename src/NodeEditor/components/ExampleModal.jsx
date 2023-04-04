e2065be06bf862a327b0142ed4a2b11417c1b29f
devin.chenyang <devin.chenyang@grabtaxi.com>
2023-04-04 21:54:52 +0800

    fix examples

--- src/NodeEditor/components/ExampleModal.jsx
+++ src/NodeEditor/components/ExampleModal.jsx
@@ -5,12 +5,12 @@ import examples from 'NodeEditor/examples';
 import { clearEditorAndMap } from 'NodeEditor/helpers';
 
 function ExampleModal({ open, onOk, onCancel }) {
-  const handleExample = (example) => () => {
+  const handleExample = (exampleFn) => () => {
     if (window.confirm('Are you sure to clear all data?') !== true) { // eslint-disable-line no-alert
       return;
     }
     clearEditorAndMap(); // clear all content on map and editor
-    examples[example](); // load the data from example to map and editor
+    exampleFn(); // load the data from example to map and editor
     onOk(); // hide modal
   };
   const es = ['Display a web map using an alternate projection', 'Add a line to a map using a GeoJSON source', 'Create and style clusters'];
@@ -19,11 +19,11 @@ function ExampleModal({ open, onOk, onCancel }) {
       <Row>
         {Object.keys(examples).map((e) => {
           if (typeof examples[e] !== 'object') {
-            return <Col key={e} span={6}><button type="button" onClick={handleExample(e)}>{ e}</button></Col>;
+            return <Col key={e} span={6}><button type="button" onClick={handleExample(examples[e])}>{e}</button></Col>;
           }
           return (
             <Col key={e} span={6}>
-              <button type="button" onClick={handleExample(e)}>{e}</button>
+              <button type="button" onClick={handleExample(examples[e].component)}>{e}</button>
               <a href={examples[e].link}>Example Link</a>
             </Col>
           );
